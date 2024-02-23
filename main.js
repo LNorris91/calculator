67eddc8d6541ef1da50ecbcb85ad5d87c064f174
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-02-23 10:14:35 -0500

    Create Calculator Functions
    create basic calculator functions that can add,
    subtract, multiply, and divide. also creates an
    operate function that executes those functions
    with the given number inputs.

---
+++ main.js
@@ -0,0 +1,22 @@
+const add = function(a, b) {
+    return a + b
+}
+const subtract = function(a, b) {
+    return a - b
+}
+const multiply = function(a, b) {
+    return a * b
+}
+const divide = function(a, b) {
+    return a / b
+}
+
+let num1
+let operator
+let num2
+
+const operate = function(operator, num1, num2) {
+   return operator(num1, num2)
+}
+
+console.log(operate(divide, 10, 5))
