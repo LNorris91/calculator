1ef625116f6dd91b629f8e49c3c0f94544a234f0
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-02-24 13:29:50 -0500

    Rewrite Algorithm to use Objects. Add new Buttons
    this commit replaces the whole Algorithm used
    for the computational portion of the code and
    replaces it with a more sturdy Algorithm using
    objoects.  this commit will also add a backspace
    button and disables the decimal button after it's
    been used once.

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const numBtn = document.querySelectorAll(".numBtn");
 const opBtn = document.querySelectorAll(".opBtn");
 const equalBtn = document.querySelector(".equalBtn");
 const clearBtn = document.querySelector(".clearBtn");
+const decimalBtn = document.querySelector("#decimal");
+const backSpaceBtn = document.querySelector(".backSpace");
+let tempEquation = {}
+let clickCount = 0
 
 const add = function(a, b) {
     return parseFloat(a) + parseFloat(b)
@@ -16,54 +20,88 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
     return parseFloat(a) / parseFloat(b)
 }
-const operate = function() {
-    let input = display.textContent.split(" ");
-    let result = 0;
-    
-    do {
-        let num1 = input[0];
-        let num2 = input[2];
-        let temp = 0
-            if (input[1] === '+'){
-                temp += add(num1, num2)
-                    if(temp % 1 !== 0){
-                        temp = temp.toFixed(2)
-                    }
-            } else if (input[1] === "-"){
-                temp += subtract(num1, num2)
-                if(temp % 1 !== 0){
-                    temp = temp.toFixed(2)
-                }
-            } else if (input[1] === "*"){
-                temp += multiply(num1, num2)
-                if(temp % 1 !== 0){
-                    temp = temp.toFixed(2)
-                }
-            } else if (input[1] === "/"){
-                temp += divide(num1, num2)
-                if(temp % 1 !== 0){
-                    temp = temp.toFixed(2)
-                }
-            }
-        input.splice(0, 3, temp)
-        result = temp
-    } while (input.length >= 3)
 
-    display.textContent = result
+const execute = function(operator, num1, num2){
+    let temp = 0;
+    let answer = 0;
+
+    if (operator === "+") {
+        temp = add(num1, num2)
+    } else if (operator === "-") {
+        temp = subtract(num1, num2)
+    } else if (operator === "*") {
+        temp = multiply(num1, num2)
+    } else if (operator === "/") {
+        temp = divide(num1, num2)
+            if (num2 == 0) {
+                temp = 0
+                alert("Don't do that")
+            }
+    }
+
+    if (temp % 1 !== 0){
+        answer = Math.round(temp * 10 ** 2) / 10 ** 2;
+    } else {
+        answer = temp
+    }
+
+    return answer
 }
 
 numBtn.forEach((button) => {button.addEventListener('click', (e) => {
+    if (clickCount == 0) {
+        display.textContent = e.target.value;
+    } else {
     display.textContent += e.target.value;
+    }
+    if (display.textContent.includes(".")) {
+        decimalBtn.disabled = true
+    }
+    clickCount++
 })})
 
 opBtn.forEach((button) => {button.addEventListener('click', (e) => {
-    display.textContent += " " + e.target.value + " ";
+    if (Object.keys(tempEquation).length >= 2) {
+        tempEquation.num2 = display.textContent;
+        display.textContent = execute(tempEquation.operator, tempEquation.num1, tempEquation.num2);
+        tempEquation.num1 = display.textContent;
+        delete tempEquation.num2;
+        tempEquation.operator = e.target.value;
+        decimalBtn.disabled = false
+        clickCount = 0
+        console.log(tempEquation)
+    } else {
+    tempEquation.num1 = display.textContent;
+    tempEquation.operator = e.target.value;
+    decimalBtn.disabled = false
+    clickCount = 0;
+    console.log(tempEquation)
+    }
 })})
 
 equalBtn.addEventListener('click', () => {
-    operate()
+    if (Object.keys(tempEquation).length == 2) {
+        tempEquation.num2 = display.textContent;
+        display.textContent = execute(tempEquation.operator, tempEquation.num1, tempEquation.num2);
+        delete tempEquation.num2;
+        delete tempEquation.num1;
+        delete tempEquation.operator;
+        decimalBtn.disabled = false
+        clickCount = 0
+    } else {
+
+    }
 })
 
 clearBtn.addEventListener('click', () => {
-    display.textContent = ""
+    display.textContent = "";
+    delete tempEquation.num2;
+    delete tempEquation.num1;
+    delete tempEquation.operator;
+    decimalBtn.disabled = false
+    clickCount = 0
+})
+
+backSpaceBtn.addEventListener('click', () => {
+    display.textContent = display.textContent.slice(0, -1)
 })
